4bbf35c10f28d4fb45c13b1de4624fd412551b2b
tiedsandi <fachransandi@gmail.com>
2025-01-04 20:20:52 +0700

    switching auth modes with query param (search params)

--- 5-auth/components/auth-form.js
+++ 5-auth/components/auth-form.js
@@ -5,7 +5,7 @@ import {useFormState} from 'react-dom';
 
 import {signup} from '../actions/auth-actions';
 
-export default function AuthForm() {
+export default function AuthForm({mode}) {
   const [formState, formAction] = useFormState(signup, {});
 
   return (
@@ -30,10 +30,15 @@ export default function AuthForm() {
         </ul>
       )}
       <p>
-        <button type='submit'>Create Account</button>
+        <button type='submit'>
+          {mode === 'login' ? 'Login' : 'Create Account'}
+        </button>
       </p>
       <p>
-        <Link href='/'>Login with existing account.</Link>
+        {mode === 'login' && <Link href='/?mode=signup'>Create an account.</Link>}
+        {mode === 'signup' && (
+          <Link href='/?mode=login'>Login with existing account.</Link>
+        )}
       </p>
     </form>
   );
