ebb7bde5ef2e07a0edf7ad5b9e75a26a149e08c8
tiedsandi <fachransandi@gmail.com>
2025-01-04 08:53:06 +0700

    finishing signup logic

---
+++ 5-auth/lib/user.js
@@ -0,0 +1,10 @@
+import db from './db';
+
+export function createUser(email, password) {
+  const result = db
+    .prepare('INSERT INTO users (email, password) VALUES (?, ?)')
+    .run(email, password);
+
+  console.log(result);
+  return result.lastInsertRowid;
+}
