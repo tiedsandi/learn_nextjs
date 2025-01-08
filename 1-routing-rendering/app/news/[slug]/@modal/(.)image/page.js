474c30857759ad0dad6b1cb7bebef1575f7c1009
tiedsandi <fachransandi@gmail.com>
2025-01-08 15:20:51 +0700

    1-navigating programmatically

--- 1-routing-rendering/app/news/[slug]/@modal/(.)image/page.js
+++ 1-routing-rendering/app/news/[slug]/@modal/(.)image/page.js
@@ -1,23 +1,14 @@
+import ClientModal from '@/components/client-modal';
 import {DUMMY_NEWS} from '@/dummy-news';
 import {notFound} from 'next/navigation';
 
 export default async function IntercepptedImagePage({params}) {
   const {slug} = await params;
-
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === slug);
 
   if (!newsItem) {
     notFound();
   }
 
-  return (
-    <>
-      <div className='modal-backdrop' />
-      <dialog className='modal' open>
-        <div className='fullscreen-image'>
-          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-        </div>
-      </dialog>
-    </>
-  );
+  return <ClientModal newsItem={newsItem} />;
 }
