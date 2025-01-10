568864788034163db527f0ee4bc4ce6b6ccc7e50
tiedsandi <fachransandi@gmail.com>
2025-01-10 08:40:13 +0700

    3-storing data in db

--- 3-data-mutation/app/new-post/page.js
+++ 3-data-mutation/app/new-post/page.js
@@ -1,12 +1,18 @@
 import {storePost} from '@/lib/posts';
 
 export default function NewPostPage() {
-  function createPost(formData) {
+  async function createPost(formData) {
+    'use server';
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    console.log(title, image, content);
+    storePost({
+      imageUrl: '',
+      title,
+      content,
+      userId: 1,
+    });
   }
 
   return (
