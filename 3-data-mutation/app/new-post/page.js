f0224f428ef0e2741f79f7ebcb6a7fff360b8d32
tiedsandi <fachransandi@gmail.com>
2025-01-10 08:32:07 +0700

    3-setting up a form action

--- 3-data-mutation/app/new-post/page.js
+++ 3-data-mutation/app/new-post/page.js
@@ -1,43 +1,37 @@
-import { storePost } from '@/lib/posts';
+import {storePost} from '@/lib/posts';
 
 export default function NewPostPage() {
-  async function createPost(formData) {
-    "use server";
+  function createPost(formData) {
     const title = formData.get('title');
     const image = formData.get('image');
     const content = formData.get('content');
 
-    storePost({
-      imageUrl: '',
-      title,
-      content,
-      userId: 1
-    })
+    console.log(title, image, content);
   }
 
   return (
     <>
       <h1>Create a new post</h1>
       <form action={createPost}>
-        <p className="form-control">
-          <label htmlFor="title">Title</label>
-          <input type="text" id="title" name="title" />
+        <p className='form-control'>
+          <label htmlFor='title'>Title</label>
+          <input type='text' id='title' name='title' />
         </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL</label>
+        <p className='form-control'>
+          <label htmlFor='image'>Image URL</label>
           <input
-            type="file"
-            accept="image/png, image/jpeg"
-            id="image"
-            name="image"
+            type='file'
+            accept='image/png, image/jpeg'
+            id='image'
+            name='image'
           />
         </p>
-        <p className="form-control">
-          <label htmlFor="content">Content</label>
-          <textarea id="content" name="content" rows="5" />
+        <p className='form-control'>
+          <label htmlFor='content'>Content</label>
+          <textarea id='content' name='content' rows='5' />
         </p>
-        <p className="form-actions">
-          <button type="reset">Reset</button>
+        <p className='form-actions'>
+          <button type='reset'>Reset</button>
           <button>Create Post</button>
         </p>
       </form>
