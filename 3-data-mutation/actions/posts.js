eeb38249237d28ce835527a47dfd8034d788075b
tiedsandi <fachransandi@gmail.com>
2025-01-10 10:16:44 +0700

    3-uploading&storing images

--- 3-data-mutation/actions/posts.js
+++ 3-data-mutation/actions/posts.js
@@ -1,5 +1,6 @@
 'use server';
 
+import {uploadImage} from '@/lib/cloudinary';
 import {storePost} from '@/lib/posts';
 import {redirect} from 'next/navigation';
 
@@ -26,8 +27,17 @@ export async function createPost(prevState, formData) {
     return {errors};
   }
 
+  let imageUrl;
+  try {
+    imageUrl = await uploadImage(image);
+  } catch (error) {
+    throw new Error(
+      'Image upload failed, post was not created. Please try agian later'
+    );
+  }
+
   await storePost({
-    imageUrl: '',
+    imageUrl: imageUrl,
     title,
     content,
     userId: 1,
