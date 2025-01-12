7228b4f6cffda206c8ee110a0531aa4d12c3a8a0
tiedsandi <fachransandi@gmail.com>
2025-01-12 19:21:20 +0700

    4-controlling data caching

--- 4-caching/app/messages/page.js
+++ 4-caching/app/messages/page.js
@@ -1,7 +1,13 @@
 import Messages from '@/components/messages';
 
+export const revalidate = 10;
+
 export default async function MessagesPage() {
-  const response = await fetch('http://localhost:8080/messages');
+  const response = await fetch('http://localhost:8080/messages', {
+    // next: {
+    //   revalidate: 10,
+    // },
+  });
   const messages = await response.json();
 
   if (!messages || messages.length === 0) {
